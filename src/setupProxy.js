82d6bbf743c8357afd8cb631911af25a206082e0
WilsonWann <37257533+WilsonWann@users.noreply.github.com>
2021-02-23 09:56:12 +0800

    1. remove tracing .yarnrc 2. modify setupProxy to proxy image

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,36 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = app => {
-    app.use(
-        '/service/API',
-        createProxyMiddleware({
-            target: 'http://dev-pc-hb.fastbet108.com',
-            changeOrigin: true,
-            logLevel: 'debug'
-        })
-    )
-};
+const proxySelector = [
+  {
+    path: "/service/API",
+    target: "http://uat-pc-hb.fastbet108.com",
+  },
+  {
+    path: "**/*.png",
+    target: "http://uat-files-hb.fastbet108.com",
+  },
+  {
+    path: "**/*.jpg",
+    target: "http://uat-files-hb.fastbet108.com",
+  },
+  // {
+  //     path: '**/*.js',
+  //     target: 'http://uat-files-hb.fastbet108.com'
+  // },
+  // {
+  //     path: '**/*.css',
+  //     target: 'http://uat-files-hb.fastbet108.com'
+  // },
+];
+
+const createProxy = (target) =>
+  createProxyMiddleware({
+    target: target,
+    changeOrigin: true,
+    logLevel: "debug",
+  });
+
+module.exports = (app) =>
+  proxySelector.forEach((proxy) =>
+    app.use(proxy.path, createProxy(proxy.target))
+  );
