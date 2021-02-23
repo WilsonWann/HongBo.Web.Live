4d6bdc5212ad6843057e49ef6bb8b2bc773c278d
WilsonWann <37257533+WilsonWann@users.noreply.github.com>
2021-02-23 13:11:06 +0800

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,27 +1,27 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const proxySelector = [
-  {
+const proxySelector = {
+  api: {
     path: "/service/API",
     target: "http://uat-pc-hb.fastbet108.com",
   },
-  {
+  png: {
     path: "**/*.png",
     target: "http://uat-files-hb.fastbet108.com",
   },
-  {
+  jpg: {
     path: "**/*.jpg",
     target: "http://uat-files-hb.fastbet108.com",
   },
-  // {
-  //     path: '**/*.js',
-  //     target: 'http://uat-files-hb.fastbet108.com'
-  // },
-  // {
-  //     path: '**/*.css',
-  //     target: 'http://uat-files-hb.fastbet108.com'
-  // },
-];
+  js: {
+    path: "**/*.js",
+    target: "http://uat-files-hb.fastbet108.com",
+  },
+  css: {
+    path: "**/*.css",
+    target: "http://uat-files-hb.fastbet108.com",
+  },
+};
 
 const createProxy = (target) =>
   createProxyMiddleware({
@@ -29,8 +29,20 @@ const createProxy = (target) =>
     changeOrigin: true,
     logLevel: "debug",
   });
+const createCDNProxy = (target) =>
+  createProxyMiddleware({
+    target: target,
+    changeOrigin: true,
+    logLevel: "debug",
+    pathRewrite: {
+      "^/assets/images/Android": "/assets_newPC/images/Android",
+    },
+  });
 
-module.exports = (app) =>
-  proxySelector.forEach((proxy) =>
-    app.use(proxy.path, createProxy(proxy.target))
-  );
+module.exports = (app) => {
+  console.log("proxySelector: ", proxySelector);
+  app.use(proxySelector.api.path, createProxy(proxySelector.api.target));
+  app.use(proxySelector.png.path, createCDNProxy(proxySelector.png.target));
+  app.use(proxySelector.jpg.path, createCDNProxy(proxySelector.jpg.target));
+  return app;
+};
